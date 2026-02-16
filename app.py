e0da79288200de6ef182a8415ec1a05309435368
adz091660-art <adz091660@gmail.com>
2026-02-16 21:47:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello AI App")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello AI Web App"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
